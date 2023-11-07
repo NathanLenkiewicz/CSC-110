e438902264eced77cd733d85f2d58d3327386410
NathanLenkiewicz <80632474+NathanLenkiewicz@users.noreply.github.com>
2023-11-06 20:01:01 -0700

    Add files via upload

--- benfords_law.py
+++ benfords_law.py
@@ -3,31 +3,45 @@ def csv_to_list(file_name):
     data = open(file_name)
     for i in data:
         list_values = i.strip().split(",")
-        if (list_values[1].isnumeric()):
-            numbers.append(list_values[1])
+        if (len(list_values) > 1):
+            if (list_values[len(list_values)-1].isnumeric()):
+                numbers.append(list_values[len(list_values)-1])
+        elif (len(list_values) <= 1):
+            if list_values[0].isnumeric():
+                numbers.append(list_values[0])
     return numbers
 
 def count_start_digits(numbers):
     occurence = {}
     for i in range(len(numbers)):
         integer_i = int(numbers[i][0])
-        if (integer_i not in occurence):
+        if (integer_i not in occurence and integer_i != 0):
             occurence[integer_i] = 1
-        else:
+        elif (integer_i in occurence and integer_i != 0):
             occurence[integer_i] += 1
     return occurence
 
-def calculate_percentages(counts):
+def digit_percentages(counts):
     percentages = {}
     sum = 0
-    for key, value in counts.items():
-        print(value)
+    for value in counts.values():
         sum += value
-    return sum
-        
+    for key, value in counts.items():
+        percentages[key] = round((value / sum) * 100, 2)
+    return percentages
 
-nums = csv_to_list("places.csv")
+def check_benfords_law(percentages):
+    percentage_range = {1: 30, 2: 17, 3: 12, 4: 9, 5: 7, 6: 6, 7: 5, 8: 5, 9: 4}
+    difference_dict = {}
+    follows_benfords_law = True
 
-occurences = count_start_digits(nums)
+    for key, value in percentages.items():
+        difference_dict[key] = round(value - percentage_range[key], 2)
 
-print(calculate_percentages(occurences))
+    for key, value in difference_dict.items():
+        if value < 2.5:
+            follows_benfords_law = True
+        else:
+            follows_benfords_law = False
+
+    return follows_benfords_law
